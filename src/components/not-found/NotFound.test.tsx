593d15f36a8c4460bc5f93ac8d91a8e15c251754
leandroyabut <leandro.yabut@smoothstack.com>
2021-07-08 17:56:48 -0600

    Change expectation for not found component.

--- src/components/not-found/NotFound.test.tsx
+++ src/components/not-found/NotFound.test.tsx
@@ -4,7 +4,7 @@ import {renderWithRouter} from "@test-utils";
 
 describe("NotFound Component", () => {
 
-    it("should contain '404' or 'Not Found' on screen", () => {
+    it("should contain '404' or 'Not Found'", () => {
         const { getByText } = renderWithRouter(<NotFound/>);
         const notFoundText = getByText(/(404|Not Found)/i);
         expect(notFoundText).toBeInTheDocument();
