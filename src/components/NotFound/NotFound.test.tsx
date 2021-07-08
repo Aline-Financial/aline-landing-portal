d8bfbc0f31627dfee5f3302e6f0ba917b070c8be
leandroyabut <leandro.yabut@smoothstack.com>
2021-07-08 12:03:26 -0600

    Add test for not found.

--- src/components/NotFound/NotFound.test.tsx
+++ src/components/NotFound/NotFound.test.tsx
@@ -1,4 +1,19 @@
-it("should c", function () {
-    
+import React from "react";
+import {render} from "@testing-library/react";
+import {NotFound} from "./NotFound";
+import {BrowserRouter as Router} from "react-router-dom";
+
+describe("NotFound Component", () => {
+
+    it("should contain '404' or 'Not Found' on screen", () => {
+        const { getByText } = render(
+            <Router>
+                <NotFound/>
+            </Router>
+        );
+        const notFoundText = getByText(/(404|Not Found)/i);
+        expect(notFoundText).toBeInTheDocument();
+    });
+
 });
 
