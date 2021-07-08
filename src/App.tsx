4e46768c6a03ff92d325067c741b3ba2a07cfae9
leandroyabut <leandro.yabut@smoothstack.com>
2021-07-07 23:11:01 -0600

    Fix bootstrap not loading the correct primary color.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./App.scss";
-import {AppNav} from "./components/AppNav/AppNav";
 import {library} from "@fortawesome/fontawesome-svg-core";
 import React from "react";
 import {IconsConfig} from "./config/icons.config";
-import {BrowserRouter as Router, Route} from "react-router-dom";
 import {AppRouter} from "./AppRouter";
 
 export class App extends React.Component<any, any> {
