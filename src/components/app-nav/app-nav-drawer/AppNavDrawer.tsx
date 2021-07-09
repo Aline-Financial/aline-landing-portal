f60c87b4602539186b30d18c24f5d40321e2e235
leandroyabut <leandro.yabut@smoothstack.com>
2021-07-09 12:52:40 -0600

    Add inset shadow for appnav drawer dropdown

--- src/components/app-nav/app-nav-drawer/AppNavDrawer.tsx
+++ src/components/app-nav/app-nav-drawer/AppNavDrawer.tsx
@@ -16,7 +16,7 @@ export class AppNavDrawer extends React.Component<any, any> {
                     {dropdown.label}
                     <FontAwesomeIcon icon={["fas", "chevron-down"]} className="mx-2"/>
                 </a>
-                <div className="collapse" id={`drawerDropdown${dropdown.label.replace(" ", "")}`}>
+                <div className="collapse drawer-dropdown" id={`drawerDropdown${dropdown.label.replace(" ", "")}`}>
                     {
                         dropdown.routes.map(route => (
                             <Link key={`drawerLink${route.label.replace(" ", "")}`} to={route.route} className="drawer-item drawer-item-link ps-4 p-3" data-bs-dismiss="offcanvas">{route.label}</Link>
@@ -42,17 +42,17 @@ export class AppNavDrawer extends React.Component<any, any> {
                         this.generateDrawerDropdowns(AppNavRoutes)
                     }
                     </div>
-                    <div className="row row-cols-2 pb-5">
-                        <div className="col">
-                            <a href={`${process.env.REACT_APP_MEMBER_DASHBOARD_URL}/login`}>
-                                <button className="btn btn-outline-light w-100">Log In</button>
-                            </a>
-                        </div>
-                        <div className="col">
-                            <Link to={"/signup"}>
-                                <button className="btn btn-primary w-100" data-bs-dismiss="offcanvas">Sign Up</button>
-                            </Link>
-                        </div>
+                </div>
+                <div className="row row-cols-2 p-4">
+                    <div className="col">
+                        <a href={`${process.env.REACT_APP_MEMBER_DASHBOARD_URL}/login`}>
+                            <button className="btn btn-outline-light w-100">Log In</button>
+                        </a>
+                    </div>
+                    <div className="col">
+                        <Link to={"/signup"}>
+                            <button className="btn btn-primary w-100" data-bs-dismiss="offcanvas">Sign Up</button>
+                        </Link>
                     </div>
                 </div>
             </div>
