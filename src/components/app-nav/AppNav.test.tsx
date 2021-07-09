d572b6533d0eafb6528e469a7123c19c9c1a33ca
leandroyabut <leandro.yabut@smoothstack.com>
2021-07-08 23:48:45 -0600

    Add test coverage for AppNav.

--- src/components/app-nav/AppNav.test.tsx
+++ src/components/app-nav/AppNav.test.tsx
@@ -1,42 +1,57 @@
 import React from "react";
 import {renderWithRouter} from "@test-utils";
 import {AppNav} from "./AppNav";
-import assert from "assert";
 
-describe("NavBrandLink", () => {
+describe("AppNav", () => {
+    describe("NavBrandLink", () => {
+        const { getByText } = renderWithRouter(<AppNav/>);
+        const appLink = getByText((content) => {
+            return content.includes("Aline") && content.includes("Financial");
+        });
 
-    const { getByText } = renderWithRouter(<AppNav/>);
-    const appLink = getByText((content) => {
-        return content.includes("Aline") && content.includes("Financial");
-    });
+        it("should contain class name 'navbar-brand'", () => {
+            expect(appLink.classList.contains("navbar-brand")).toBe(true);
+        });
 
-    it("should contain class name 'navbar-brand'", () => {
-        expect(appLink.classList.contains("navbar-brand")).toBe(true);
-    });
+        it("should contain the 'AlineFinancial' as text", () => {
+            expect(appLink.textContent).toContain("AlineFinancial");
+        });
 
-    it("should contain the 'AlineFinancial' as text", () => {
-        expect(appLink.textContent).toContain("AlineFinancial");
-    });
+        it("should have an href of '/'", () => {
+            expect(appLink.getAttribute("href")).toBe("/");
+        });
 
-    it("should have an href of '/'", () => {
-        expect(appLink.getAttribute("href")).toBe("/");
-    });
-
-    it("should contain a non-draggable image that represents the logo", () => {
-        const logo = appLink.querySelector("img");
-        expect(logo).toBeTruthy();
-        assert(logo, "Logo was null");
-        expect(logo.getAttribute("draggable")).toBe("false");
-        expect(logo.getAttribute("src")).toBe("image test stub");
-    });
-
-});
-
-describe("NavBarNav", () => {
-    const nav = document.querySelector("ul.navbar-nav");
-
-    it("should contain a 'Login' button that links to REACT_APP_MEMBER_DASHBOARD_URL", () => {
+        it("should contain a non-draggable image that represents the logo", () => {
+            const logo = appLink.querySelector("img");
+            expect(logo).toBeTruthy();
+            expect(logo?.getAttribute("draggable")).toBe("false");
+            expect(logo?.getAttribute("src")).toBe("image test stub");
+        });
 
     });
 
+    describe("Buttons", () => {
+        const {getAllByText} = renderWithRouter(<AppNav/>);
+        const loginButtons = getAllByText(/Log In/i);
+        const signUpButtons = getAllByText(/Sign Up/i);
+
+        it("should contain 'Login' buttons that links to external member dashboard '/login'", () => {
+            expect(loginButtons.length).toBeGreaterThan(0);
+            loginButtons.forEach(loginButton => {
+                expect(loginButton.textContent).toBe("Log In");
+                const {parentElement} = loginButton;
+                expect(parentElement?.getAttribute("href")).toContain("/login");
+            });
+        });
+
+        it("should contain 'Sign Up' buttons that links to external member dashboard '/login'", () => {
+            expect(signUpButtons.length).toBeGreaterThan(0);
+            signUpButtons.forEach(signUpButton => {
+                expect(signUpButton.textContent).toBe("Sign Up");
+                const {parentElement} = signUpButton;
+                expect(parentElement?.getAttribute("href")).toContain("/signup");
+            });
+        });
+
+    });
 });
---
+++ src/components/app-nav/app-nav-drawer/AppNavDrawer.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {renderWithRouter} from "@test-utils";
+
+describe("AppNavDrawer", () => {
+
+
+
+});
