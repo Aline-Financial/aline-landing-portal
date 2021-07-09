99c684908f3fe71d082ec13d4bc4b139c22c750a
leandroyabut <leandro.yabut@smoothstack.com>
2021-07-09 01:04:33 -0600

    Update test util method.

--- src/utils/test.utils.tsx
+++ src/utils/test.utils.tsx
@@ -1,7 +1,7 @@
-import {JSXElementConstructor, ReactElement} from "react";
+import {ReactElement} from "react";
 import {render} from "@testing-library/react";
 import {MemoryRouter} from "react-router";
 
-export function renderWithRouter(ui: ReactElement<any, string | JSXElementConstructor<any>>) {
+export function renderWithRouter(ui: ReactElement) {
     return render(ui, { wrapper: MemoryRouter});
 }
