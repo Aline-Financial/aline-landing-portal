0e87e5e1846c21d27ddeb487bc3272c2021264e0
leandroyabut <leandro.yabut@smoothstack.com>
2021-07-09 12:56:18 -0600

    Add pages directory.

---
+++ src/pages/landing/Landing.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class Landing extends React.Component<any, any> {
+    render() {
+        return (
+            <div className="container">
+
+            </div>
+        );
+    }
+}
