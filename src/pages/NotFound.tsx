c8e2b2d6aa4f1a42bcd85bb0fe1a5c8081cd71a5
leandroyabut <leandro.yabut@smoothstack.com>
2021-07-09 23:58:38 -0600

    Convert to functional component.

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React from "react";
 import {Link} from "react-router-dom";
 
 /**
@@ -6,18 +6,16 @@ import {Link} from "react-router-dom";
  * <p>Page to display a not found message.</p>
  * <p><em>This component uses a {@link Link}. Refer to it's documentation.</em></p>
  */
-export class NotFound extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="bg-light m-5 p-5 rounded">
-                    <h3 className="h3">404 Page Not Found</h3>
-                    <p className="my-5 lead">The page you are trying to access does not exist.</p>
-                    <Link to="/">
-                        <button type="button" className="btn btn-primary">Go Back Home</button>
-                    </Link>
-                </div>
+export const NotFound = () => {
+    return (
+        <div className="container">
+            <div className="bg-light m-5 p-5 rounded">
+                <h3 className="h3">404 Page Not Found</h3>
+                <p className="my-5 lead">The page you are trying to access does not exist.</p>
+                <Link to="/">
+                    <button type="button" className="btn btn-primary">Go Back Home</button>
+                </Link>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
