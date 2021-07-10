3674b80edbf24c03b6b7508f19b5211d33bee15f
leandroyabut <leandro.yabut@smoothstack.com>
2021-07-10 13:07:02 -0600

    Remove unneccessary ? from SignUpPage.

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -3,7 +3,7 @@ import {useLocation} from "react-router";
 
 const SignUp = () => {
     const location = useLocation<{email: string}>();
-    return <div>{location.state?.email}</div>;
+    return <div>{location.state.email}</div>;
 };
 
 export default SignUp;
