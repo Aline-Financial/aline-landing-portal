2a3cab3b618708dfc1c1e29de82e2eb461f2383b
leandroyabut <leandro.yabut@smoothstack.com>
2021-07-10 00:00:23 -0600

    Fix AppNavDropdown test.

--- src/components/AppNavDropdown.test.tsx
+++ src/components/AppNavDropdown.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {AppNavDropdown} from "./AppNavDropdown";
 import {renderWithRouter} from "@test-utils";
-import {Interfaces} from "@interfaces/interfaces";
+import {AppNavRoute} from "@interfaces";
 
 it("should create anchor element with href of '/'", () => {
     const {getByText} = renderWithRouter(<AppNavDropdown label={"Label"} routes={[]}/>);
@@ -21,7 +21,7 @@ it("should create anchor element with data-bs-toggle of 'dropdown'", () => {
 
 it("dropdown should generate a ul with x amount of list items", () => {
     const randomNum = Math.floor(Math.random() * 20);
-    const routes: Interfaces[] = Array.from(Array(randomNum).keys())
+    const routes: AppNavRoute[] = Array.from(Array(randomNum).keys())
         .map(i => ({
             route: `/r${i}`,
             label: `R${i}`
