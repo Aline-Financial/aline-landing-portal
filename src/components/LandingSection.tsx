1094ba6eb88562daa6f6269a2c39d556374fca68
leandroyabut <leandro.yabut@smoothstack.com>
2021-07-11 02:47:55 -0600

    Add landing section.

---
+++ src/components/LandingSection.tsx
@@ -0,0 +1,37 @@
+import React, {ReactFragment} from "react";
+import "./LandingSection.sass";
+import {Link} from "react-router-dom";
+
+type LandingSectionProps = {
+    image: string;
+    title: ReactFragment;
+    body: ReactFragment;
+    buttonText: string;
+    buttonRoute: string;
+    align: "start" | "end";
+    light: boolean;
+};
+
+const LandingSection = (props: LandingSectionProps) => {
+    const {image, title, body, buttonText, buttonRoute, align, light} = props;
+    return (
+        <div className={`container-fluid ${light ? "bg-light " : " "}py-section`}>
+            <div className="container">
+                <div className="row row-cols-1 row-cols-md-2">
+                    <div className={`col ${align == "end" ? "order-md-1" : ""}`}>
+                        <img draggable={false} src={image} className="img-fluid" alt={image}/>
+                    </div>
+                    <div className={`col ${align == "end" ? "order-md-0" : ""} text-md-${align == "end" ? "start" : "end"} text-start mt-5 mt-md-0`}>
+                        <h2 className="display-6 fw-bold">{title}</h2>
+                        <p className="lead">{body}</p>
+                        <Link to={buttonRoute}>
+                            <button className="btn btn-lg btn-primary my-md-5">{buttonText}</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LandingSection;
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import creditCard from "@images/credit_card.png";
 import LandingStart from "@components/LandingStart";
 import LandingCards from "@components/LandingCards";
+import LandingSection from "@components/LandingSection";
 
 /**
  * <strong>Page - Landing</strong>
@@ -11,11 +13,22 @@ import LandingCards from "@components/LandingCards";
  * </ul>
  */
 export const Landing = () => {
+
+    const platinumCardTitle = <>Get Approved for <span className="text-primary">Platinum</span></>;
+    const platinumCardBody = <>Apply for a checking account today and get approved for an <span className="text-primary fw-bold">Aline Financial Platinum</span> credit card with <span className="text-primary">APR as low as 3%.</span></>;
+
     return (
         <div className="container-fluid">
             <LandingStart learnMoreHref="#learn-more"/>
             <a id="learn-more"/>
             <LandingCards/>
+            <LandingSection image={creditCard}
+                            title={platinumCardTitle}
+                            body={platinumCardBody}
+                            buttonText="Get Started"
+                            buttonRoute="/signup"
+                            align="start"
+                            light={false}/>
         </div>
     );
 };
